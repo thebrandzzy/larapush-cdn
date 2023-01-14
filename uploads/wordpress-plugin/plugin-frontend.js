5942e3c4677ecef9f163701fb5061219b1a64217
Satyam <satyam.gupta756@gmail.com>
2023-01-15 00:15:56 +0530

    updated script

--- uploads/wordpress-plugin/plugin-frontend.js
+++ uploads/wordpress-plugin/plugin-frontend.js
@@ -1,12 +1,18 @@
-// img = document.createElement('img')
-// img.src = "https://raw.githubusercontent.com/larapush/larapush/main/ads/timeToCleanupYourPanel.gif"
-// img.style.width='100%'
-// img.style.maxWidth='800px'
-// img.style.marginBottom = "10px"
+(() => {
+	img = document.createElement("img");
+	img.src = "https://raw.githubusercontent.com/larapush/larapush/main/ads/timeToCleanupYourPanel.gif";
+	img.style.width = "100%";
+	img.style.maxWidth = "800px";
 
-// div = document.createElement('div')
-// div.style.display = "flex"
-// div.style.justifyContent = "center"
-// div.append(img)
+	a = document.createElement("a");
+	a.href = "https://larapush.com/?utm_source=wordpress&utm_medium=plugin&utm_campaign=cleanup";
+	a.target = "_blank";
+	a.append(img);
 
-// document.querySelector('main').prepend(div)
+	div = document.createElement("div");
+	div.style.display = "flex";
+	div.style.justifyContent = "center";
+	div.append(a);
+
+	document.querySelector("main").prepend(div);
+})();
