cfab91e590e0e0ec302643a8c57caed8ead4c0b3
Satyam <satyam.gupta756@gmail.com>
2023-01-15 03:09:55 +0530

    updated ad config

--- uploads/wordpress-plugin/pages-frontend.js
+++ uploads/wordpress-plugin/pages-frontend.js
@@ -1,2 +1,17 @@
-const timer = 5;
-const advert = '<a href="https://larapush.com/?utm_source=wordpress&utm_medium=plugin&utm_campaign=cleanup" target="_blank"><img src="https://raw.githubusercontent.com/larapush/larapush/main/ads/timeToCleanupYourPanel.gif" style="max-width: 100%; margin-top: 20px"></a>';
+function getLarapushAdConfig() {
+	const timer_to_be = 5;
+	const advert_to_be = '<a href="https://larapush.com/?utm_source=wordpress&utm_medium=plugin&utm_campaign=cleanup" target="_blank"><img src="https://raw.githubusercontent.com/larapush/larapush/main/ads/timeToCleanupYourPanel.gif" style="max-width: 100%; margin-top: 20px"></a>';
+	const targetClicksCount = 2;
+	const adInterval = 20;
+	const ad_id = 48348;
+	const currentClicksCount = localStorage.getItem("larapush_ad_" + ad_id + "_clicks") || 0;
+
+	return {
+		timer_to_be,
+		advert_to_be,
+		targetClicksCount,
+		adInterval,
+		ad_id,
+		currentClicksCount,
+	};
+}
