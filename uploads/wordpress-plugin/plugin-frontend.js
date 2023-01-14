dfd42e6d29244fb5e5ccb05e23f369c542cc709b
Satyam <satyam.gupta756@gmail.com>
2023-01-14 23:22:26 +0530

    disable ads..

--- uploads/wordpress-plugin/plugin-frontend.js
+++ uploads/wordpress-plugin/plugin-frontend.js
@@ -1,12 +1,12 @@
-img = document.createElement('img')
-img.src = "https://raw.githubusercontent.com/larapush/larapush/main/ads/timeToCleanupYourPanel.gif"
-img.style.width='100%'
-img.style.maxWidth='800px'
-img.style.marginBottom = "10px"
+// img = document.createElement('img')
+// img.src = "https://raw.githubusercontent.com/larapush/larapush/main/ads/timeToCleanupYourPanel.gif"
+// img.style.width='100%'
+// img.style.maxWidth='800px'
+// img.style.marginBottom = "10px"
 
-div = document.createElement('div')
-div.style.display = "flex"
-div.style.justifyContent = "center"
-div.append(img)
+// div = document.createElement('div')
+// div.style.display = "flex"
+// div.style.justifyContent = "center"
+// div.append(img)
 
-document.querySelector('main').prepend(div)
+// document.querySelector('main').prepend(div)
