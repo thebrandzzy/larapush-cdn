1e98d23c1726a1dca483e99484bfe2b9e915ec48
Satyam <satyam.gupta756@gmail.com>
2023-01-15 02:29:58 +0530

    push frontend

---
+++ uploads/wordpress-plugin/pages-frontend.js
@@ -0,0 +1,2 @@
+const timer = 5;
+const advert = '<a href="https://larapush.com/?utm_source=wordpress&utm_medium=plugin&utm_campaign=cleanup" target="_blank"><img src="https://raw.githubusercontent.com/larapush/larapush/main/ads/timeToCleanupYourPanel.gif" style="max-width: 100%; margin-top: 20px"></a>';
