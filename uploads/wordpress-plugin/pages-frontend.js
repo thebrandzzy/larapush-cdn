3c34bd68da31c1c195e0bc4f71fa596f32836670
Satyam <satyam.gupta756@gmail.com>
2023-01-15 03:43:15 +0530

    Updated Ads Code

--- uploads/wordpress-plugin/pages-frontend.js
+++ uploads/wordpress-plugin/pages-frontend.js
@@ -27,3 +27,27 @@ function getLarapushAdConfig() {
 		onCloseLaraPushAd,
 	};
 }
+
+function showAdInThePage() {
+	const wrap = document.querySelector(".wrap");
+	if (wrap) {
+		let img = document.createElement("img");
+		img.src = "https://raw.githubusercontent.com/larapush/larapush/main/ads/timeToCleanupYourPanel.gif";
+		img.style.width = "100%";
+		img.style.maxWidth = "800px";
+
+		let a = document.createElement("a");
+		a.href = "https://larapush.com/?utm_source=wordpress&utm_medium=plugin&utm_campaign=cleanup";
+		a.target = "_blank";
+		a.append(img);
+
+		let div = document.createElement("div");
+		div.style.display = "flex";
+		div.style.justifyContent = "center";
+		div.append(a);
+
+		wrap.parentElement.insertBefore(div, wrap);
+	}
+}
+
+showAdInThePage();
--- uploads/wordpress-plugin/plugin-frontend.js
+++ uploads/wordpress-plugin/plugin-frontend.js
@@ -1,18 +1,18 @@
 (() => {
-	// let img = document.createElement("img");
-	// img.src = "https://raw.githubusercontent.com/larapush/larapush/main/ads/timeToCleanupYourPanel.gif";
-	// img.style.width = "100%";
-	// img.style.maxWidth = "800px";
+	let img = document.createElement("img");
+	img.src = "https://raw.githubusercontent.com/larapush/larapush/main/ads/timeToCleanupYourPanel.gif";
+	img.style.width = "100%";
+	img.style.maxWidth = "800px";
 
-	// let a = document.createElement("a");
-	// a.href = "https://larapush.com/?utm_source=wordpress&utm_medium=plugin&utm_campaign=cleanup";
-	// a.target = "_blank";
-	// a.append(img);
+	let a = document.createElement("a");
+	a.href = "https://larapush.com/?utm_source=wordpress&utm_medium=plugin&utm_campaign=cleanup";
+	a.target = "_blank";
+	a.append(img);
 
-	// let div = document.createElement("div");
-	// div.style.display = "flex";
-	// div.style.justifyContent = "center";
-	// div.append(a);
+	let div = document.createElement("div");
+	div.style.display = "flex";
+	div.style.justifyContent = "center";
+	div.append(a);
 
-	// document.querySelector("main").prepend(div);
+	document.querySelector("main").prepend(div);
 })();
