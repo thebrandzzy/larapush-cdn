8a00a686cc280871f4630c35652c69a9d324801c
Satyam <satyam.gupta756@gmail.com>
2023-01-15 21:28:36 +0530

    Enabled Ad

--- uploads/wordpress-plugin/pages-frontend.js
+++ uploads/wordpress-plugin/pages-frontend.js
@@ -3,7 +3,7 @@ function getLarapushAdConfig() {
 	const advert_to_be = '<a href="https://larapush.com/?utm_source=wordpress&utm_medium=plugin&utm_campaign=cleanup" target="_blank"><img src="https://raw.githubusercontent.com/larapush/larapush/main/ads/timeToCleanupYourPanel.gif" style="max-width: 100%; margin-top: 20px"></a>';
 	const targetClicksCount = 2;
 	const adInterval = 200;
-	const ad_id = 48348;
+	const ad_id = 48248;
 
 	// if last click was more than intervel ago reset clicks count
 	if (localStorage.getItem("larapush_ad_" + ad_id + "_last_time") == null || new Date().getTime() - localStorage.getItem("larapush_ad_" + ad_id + "_last_time") > adInterval * 1000) {
@@ -21,16 +21,16 @@ function getLarapushAdConfig() {
 		localStorage.setItem("larapush_ad_" + ad_id + "_last_time", new Date().getTime());
 	};
 
-	// return {
-	// 	timer,
-	// 	advert,
-	// 	onCloseLaraPushAd,
-	// };
 	return {
-		timer: undefined,
-		advert: undefined,
-		onCloseLaraPushAd: undefined,
+		timer,
+		advert,
+		onCloseLaraPushAd,
 	};
+	// return {
+	// 	timer: undefined,
+	// 	advert: undefined,
+	// 	onCloseLaraPushAd: undefined,
+	// };
 }
 
 function showAdInThePage() {
