f2234fbfe8a8acca51d98995ebfd57c42055e853
LaraPush - Self-Hosted WebPush Panel <96669628+larapush@users.noreply.github.com>
2023-06-15 20:49:39 +0530

    Added Random Wallpaper Element to v3.js

--- uploads/v3.js
+++ uploads/v3.js
@@ -1,1 +1,5 @@
-console.log('Hello')
+let wallpaperElement = document.querySelector('.auth.login-bg');
+if(wallpaperElement) {
+    wallpaperElement.style.background = 'url(https://source.unsplash.com/1920x1080/?wallpaper)';
+    wallpaperElement.style.backgroundSize = 'cover';
+}
